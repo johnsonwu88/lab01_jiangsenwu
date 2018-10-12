eacfa0679c57a9b53df614f8743f8b141d9d51a3
johnsonwu88 <43834338+johnsonwu88@users.noreply.github.com>
2018-10-12 16:34:28 -0700

    Add files via upload

--- pi.cpp
+++ pi.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(){
   int n;
   int x;
-  double pi:
+  double pi;
     cout.setf(ios::fixed);
     cout.setf(ios::showpoint);
     cout.precision(3);
@@ -18,8 +18,8 @@ int main(){
       if(n>0){
 	pi=0;
 	for (x=0;x<=n;x++){
-	  pi=pi+(4*(pow(-1,x)/(2*x+1));}
-	}
+	  pi=pi+(4*(pow(-1,x)/(2*x+1)));}
+      }
 	cout << "The approximate value of pi using " << n+1 << " terms is: "<<pi<<endl;}
       while(n!=-1);
 

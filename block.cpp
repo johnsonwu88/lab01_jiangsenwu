6713bccab95ff63e43d7a8bb39daf49add2465fe
johnsonwu88 <43834338+johnsonwu88@users.noreply.github.com>
2018-10-12 16:24:06 -0700

    Add files via upload

--- block.cpp
+++ block.cpp
@@ -5,16 +5,16 @@ using namespace std;
 
 
 int main(){
-  int a;
-  int b;
-  cout << "Enter number of rows and columns:" << endl;
-  cin >> a >>  b;
-  for (int i = 0; i<a; i++){
-    for (int j = 0; j<b; j++){
-      cout << "X.";
-    }
-    cout << endl;
+  int rows;
+  int columns;
+  do{
+    cout << "Enter number of rows and columns:" << endl;
+    cin >> rows >> columns;
+    for (int i = 0; i<rows; i++){
+        for (int j = 0; j<columns; j++){
+          cout << "X.";
+	}
+	cout << endl;}
   }
-
-  return 0;
+  while (rows!=0 and columns!=0);
 }
--- pi.cpp
+++ pi.cpp
@@ -1,1 +1,30 @@
-sfljhlashflkashlfhklashfh
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+int main(){
+  int n;
+  int x;
+  double pi:
+    cout.setf(ios::fixed);
+    cout.setf(ios::showpoint);
+    cout.precision(3);
+
+    do{
+      cout << "Enter the value of the parameter 'n' in the Leibniz formula(or -1 to quit):" << endl;
+      cin >> n;
+      if(n==0){
+	cout << "The approximate value of pi using 1 term is: 4.000" << endl;}
+      if(n>0){
+	pi=0;
+	for (x=0;x<=n;x++){
+	  pi=pi+(4*(pow(-1,x)/(2*x+1));}
+	}
+	cout << "The approximate value of pi using " << n+1 << " terms is: "<<pi<<endl;}
+      while(n!=-1);
+
+
+
+      return 0;
+
+    }
